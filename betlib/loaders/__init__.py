3642a605f970ff7f66ec3033fec375203ad2922b
Alex Kosh <sasha23969@gmail.com>
2020-02-16 21:36:40 +0300

    feat: Module init added

---
+++ betlib/loaders/__init__.py
@@ -0,0 +1,6 @@
+from betlib.loaders.abstract import AbstractGateway
+from betlib.loaders.onexbet import Gateway1xBet
+from betlib.loaders.bingoboom import GatewayBingoboom
+from betlib.loaders.fonbet import GatewayFonbet
+
+__all__ = ["AbstractGateway", "Gateway1xBet", "GatewayBingoboom", "GatewayFonbet"]
